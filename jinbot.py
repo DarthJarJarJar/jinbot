e422d59ed48f6ecefa31c30d275de72e2388e231
DarthJarJarJar <88474643+DarthJarJarJar@users.noreply.github.com>
2021-08-13 10:15:51 +0530

    Update jinbot.py

--- jinbot.py
+++ jinbot.py
@@ -4,6 +4,8 @@ from discord.utils import get
 import requests
 from bs4 import BeautifulSoup
 from selenium import webdriver
+import json
+
 
 import re
 
@@ -164,6 +166,50 @@ async def on_member_join(member):
         "<:Wjin:865274048988184588><:Ejin:865274113174405131><:Ljin:865274170157432843><:Cjin:865274259353370634><:Ojin:865274346129850408><:Mjin:865274436168450058><:Ejin:865274113174405131>")
     role = get(member.guild.roles, id=833781809128669265)
     await member.add_roles(role)
+    
+    with open('users.json', 'r') as f:
+        users = json.load(f)
+
+    await update_data(users, member)
+
+    with open('users.json', 'w') as f:
+        json.dump(users, f, indent=4)
+    await client.process_commands(message)
+    
+    
+async def update_data(users,user):
+    if not f'(user.id)' in users:
+        users[f'{user.id}'] = {}
+        users[f'{user.id}']['experience'] = 0
+        users[f'{user.id}']['level'] = 1
+
+async def add_experience(users, user, exp):
+    users[f'{user.id}']['experience'] += exp
+
+
+async def level_up(users, user, message):
+    with open('levels.json', 'r') as g:
+        levels = json.load(g)
+
+    experience = users[f'{user.id}']['experience']
+    lvl_start = users[f'{user.id}']['level']
+    lvl_end = int(experience ** (1/4))
+    if lvl_start < lvl_end:
+        await message.channel.send(f'{user.mention} has leveled up. Level {lvl_end}')
+        users[f'{user.id}']['level'] = lvl_end
+
+@client.command()
+async def xp(ctx, member : discord.Member = None):
+    if member == None:
+        member = ctx.author()
+
+    id = member.id
+    with open('users.json', 'r') as f:
+        users = json.load(f)
+    exper = users[str(id)]['experience']
+    await ctx.send(f'You have {exper} xp')
+
+
 
 
 @client.event
