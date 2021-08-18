00cd2db9ce8ae29c111826fbf0844ae8f8806f9d
DarthJarJarJar <88474643+DarthJarJarJar@users.noreply.github.com>
2021-08-18 11:12:59 +0530

    Update jin.py

--- jin.py
+++ jin.py
@@ -71,9 +71,12 @@ async def on_member_remove(member):
 
 @client.command()
 async def jin(ctx, num=1):
-    for i in range(num):
+    if num>25:
+        await ctx.send("Please use a value less than 25")
+    else:
+        for i in range(num):
         
-        await ctx.send('<:jinhappy1:835921639551008818>')
+            await ctx.send('<:jinhappy1:835921639551008818>')
 
 
 @client.command()
