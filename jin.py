7c09146620c6ed5e0300ea4123927b10774ca985
DarthJarJarJar <88474643+DarthJarJarJar@users.noreply.github.com>
2021-09-10 00:31:45 +0530

    Update jin.py

--- jin.py
+++ jin.py
@@ -238,7 +238,7 @@ async def jin(ctx, num=1):
 @client.command
 @command.has_any_role("Jinderator", "Adjin")
 async def mute(ctx, ussr : discord.User):
-    await await ussr.add_roles(discord.utils.get(ussr.guild.roles, name="Mute"))
+    await ussr.add_roles(discord.utils.get(ussr.guild.roles, name="Mute"))
     await ctx.send("User was muted")
         
      
