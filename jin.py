9c85ea5929b4f9b40569a0c059511448e6a858cb
DarthJarJarJar <88474643+DarthJarJarJar@users.noreply.github.com>
2021-09-14 19:02:55 +0530

    Update jin.py

--- jin.py
+++ jin.py
@@ -7,6 +7,7 @@ import levelsys
 import requests
 from bs4 import BeautifulSoup
 from selenium import webdriver
+import asyncio
 
 import re
 
@@ -304,6 +305,64 @@ async def freeping(ctx):
 @client.command()
 async def language(ctx):
     await ctx.send("من فضلك لا تستخدم أي لغة أخرى غير Jin")
+    
 
 
+@client.command(aliases=['Mute'])
+@commands.has_permission(manage_messages=True)
+async def mute(ctx, member: discord.Member=None, time=None, *, reason=None):
+if not member:
+	await ctx.send("mention a member to mute")
+elif not time:
+	await ctx.send("mention mute duration")
+else:
+	if not reason:
+   		reason="No reason given"
+    #Now timed mute manipulation
+    try:
+    	seconds = time[:-1] #Gets the numbers from the time argument, start to -1
+        duration = time[-1] #Gets the timed maniulation, s, m, h, d
+        if duration == "s":
+        	seconds = seconds * 1
+        elif duration == "m":
+        	seconds = seconds * 60
+        elif duration == "h":
+        	seconds = seconds * 60 * 60
+        elif duration == "d":
+        	seconds = seconds * 86400
+        else:
+        	await ctx.send("Invalid duration input")
+          	return
+    except Exception as e:
+    	print(e)
+        await ctx.send("Invalid time input")
+        return
+    guild = ctx.guild
+   	Muted = discord.utils.get(guild.roles, name="Muted")
+    if not Muted:
+    	Muted = await guild.create_role(name="Muted")
+        for channel in guild.channels:
+        await channel.set_permissions(mutedRole, speak=False, send_messages=False, read_message_history=True, read_messages=False)
+    await member.add_roles(Muted, reason=reason)
+    muted_embed = discord.Embed(title="Muted a user", description=f"{member.mention} Was muted by {ctx.author.mention} for {reason} to {time}")
+    await ctx.send(embed=muted_embed)
+    await asyncio.sleep(seconds)
+  	await member.remove_roles(Muted)
+    unmute_embed = discord.Embed(title="Unmuted user", description=f'{ctx.author.mention} muted to {member.mention} for {reason} is over after {time}")
+    await ctx.send(embed=unmute_embed)
+
+                                 
+                               
+                                 
+@client.command(description="Unmutes a specified user.")
+@commands.has_permissions(manage_messages=True)
+async def unmute(ctx, member: discord.Member):
+   mutedRole = discord.utils.get(ctx.guild.roles, name="Muted")
+
+   await member.remove_roles(mutedRole)
+   #await member.send(f" you have unmutedd from: - {ctx.guild.name}")
+   embed = discord.Embed(title="unmute", description=f" unmuted-{member.mention}",colour=discord.Colour.light_gray())
+   await ctx.send(embed=embed)                                 
+                                 
+                                 
 client.run('ODcyMTk0NzA3MTE0MDQ1NDQw.YQmUng.NX93HMWlDNvAmPPMQVqix1kasNg')
