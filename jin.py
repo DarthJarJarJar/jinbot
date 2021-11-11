6403b31241b667fd2104ee755a7a27fe429db085
DarthJarJarJar <ayaan.shahab@outlook.com>
2021-11-10 23:10:12 -0500

    added required parameter to avatar

--- jin.py
+++ jin.py
@@ -664,7 +664,7 @@ async def _colour(ctx:SlashContext):
     await button_ctx.edit_origin(content="Added the color role!")
 
 
-@slash.slash(name="avatar",description="Shows user avatar", options=[create_option(name="user",description="Select user",option_type=6)])
+@slash.slash(name="avatar",description="Shows user avatar", options=[create_option(name="user",description="Select user",option_type=6,required=False)])
 async def _avatar(ctx:SlashContext, user : discord.User):
     if user == None:
         await ctx.send(ctx.author.avatar_url)
