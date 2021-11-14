0df95f31738bf1dd81e65866ce0ab83cc09f0834
DarthJarJarJar <ayaan.shahab@outlook.com>
2021-11-14 18:48:14 -0500

    please work

--- tags.py
+++ tags.py
@@ -139,6 +139,17 @@ class tags(commands.Cog):
                 await ctx.send('<:jinhappy1:835921639551008818>')
         
 
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
     @cog_ext.cog_slash(name='xbox',guild_ids=guilds)
     async def xbox(self,ctx):
         await ctx.send('better')
@@ -150,56 +161,56 @@ class tags(commands.Cog):
         
 
     @cog_ext.cog_slash(name='fortnite',guild_ids=guilds)
-    async def fortnite(self,ctx):
+    async def _fortnite(self,ctx):
         await ctx.send('https://tenor.com/view/kratos-kratos-fortnite-fortnite-fortnite-dance-kratos-fortnite-dance-gif-19435698')
         
         
     @cog_ext.cog_slash(name='gamepass',guild_ids=guilds)
-    async def gamepass(self,ctx):
+    async def _gamepass(self,ctx):
         await ctx.send('game pass has over 300 games to play at a low fee of $15 a month and your first 3 months are only a dollar for new members')
         
     @cog_ext.cog_slash(name='tasm2',guild_ids=guilds)
-    async def tasm2(self,ctx):
+    async def _tasm2(self,ctx):
         await ctx.send("Some day, humanity will reach self individualization. Some day we'll move beyond petty scrabbles over the three poisons of life (greed, ignorance, and hatred). Some day, we will have reached a state where we're enlightened enough to watch TASM2 without calamity befalling us.")
 
     @cog_ext.cog_slash(name='rickroll',guild_ids=guilds)
-    async def rickroll(self,ctx):
+    async def _rickroll(self,ctx):
         await ctx.send('https://tenor.com/view/dance-moves-dancing-singer-groovy-gif-17029825')
         
     @cog_ext.cog_slash(name='ratchetandclank',guild_ids=guilds)
-    async def ratchetandclank(self,ctx):
+    async def _ratchetandclank(self,ctx):
         await ctx.send("They took the great story of the first game and completely cut out the interesting socioeconomic commentary and themes and satire of corporatism and shoved in the villain from the other franchise's games")
 
     @cog_ext.cog_slash(name='rule16',guild_ids=guilds)
-    async def rule16(self,ctx):
+    async def _rule16(self,ctx):
         await ctx.send('https://tenor.com/view/dead-chat-passione-admin-passione-jojolion-gif-19211422')
     @cog_ext.cog_slash(name='mole',guild_ids=guilds)
-    async def mole(self,ctx):
+    async def _mole(self,ctx):
         await ctx.send(" I'm going to reveal the truth.\n\n I am the mole.\n\n I in truth actually work with the aquatic animal in American spy ops to keep our country safe, and the government has declared this server a danger of the highest degree. It is to this end that I must monitor this server to make sure it doesn't invoke a third world war. It was for my country. It was for my duty.\n\nGod bless America.")
 
     @cog_ext.cog_slash(name='electro',guild_ids=guilds)
-    async def electro(self,ctx):
+    async def _electro(self,ctx):
         await ctx.send('https://tenor.com/view/amazing-spiderman2vomit-electro-amazing-spiderman2-spiderman-vomit-sex-gif-13866212')
         
     @cog_ext.cog_slash(name='electrocursed',guild_ids=guilds)
-    async def electrocursed(self,ctx):
+    async def _electrocursed(self,ctx):
         await ctx.send('https://cdn.discordapp.com/attachments/826766972204744767/888332410046001182/resize.gif')
         
     @cog_ext.cog_slash(name='freeping',guild_ids=guilds)
-    async def freeping(self,ctx):
+    async def _freeping(self,ctx):
         await ctx.send(f"Here's your free ping! {ctx.author.mention}")
         
     @cog_ext.cog_slash(name='language',guild_ids=guilds)
-    async def language(self,ctx):
+    async def _language(self,ctx):
         await ctx.send("من فضلك لا تستخدم أي لغة أخرى غير Jin")
         
     @cog_ext.cog_slash(name='chatting',guild_ids=guilds)
-    async def chatting(self,ctx):
+    async def _chatting(self,ctx):
         await ctx.send("Hey man, so I was thinking what if we make a server that is dedicated to chatting? As this one always seems to be dead :skull: @DarthJinJin")
         
         
     @cog_ext.cog_slash(name='flameon',description="Flame on and on and on and on!",guild_ids=guilds)
-    async def flameon(self,ctx):
+    async def _flameon(self,ctx):
         await ctx.send("https://media.discordapp.net/attachments/826766972204744767/888296963517333564/flameon.gif")
         
             
