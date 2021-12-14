64e9f082ee66ddaf57283628bf7673738693c325
DarthJarJarJar <88474643+DarthJarJarJar@users.noreply.github.com>
2021-12-14 18:19:14 -0500

    commit

--- jin.py
+++ jin.py
@@ -514,12 +514,19 @@ async def game(ctx,*,name:str):
     desc = driver.find_element_by_css_selector(".product_summary > span:nth-child(2) > span:nth-child(1) > span:nth-child(1)")
     description = desc.text
     title = driver.find_element_by_css_selector("a.hover_none > h1:nth-child(1)")
-    
+    try: 
+        driver.implicitly_wait(3)
+        toggle = driver.find_element_by_css_selector(".product_summary > span:nth-child(2) > span:nth-child(1) > span:nth-child(4)")
+        toggle.click()
+        fulldesc = driver.find_element_by_css_selector(".inline_expanded > span:nth-child(2)")
+    except NoSuchElementException:
+        fulldesc = driver.find_element_by_css_selector(".product_summary > span:nth-child(2) > span:nth-child(1)")
 
     embed = discord.Embed(title=title.text,thumbnail=thumbnail,colour=discord.Color.green(),url=result)
-    embed.add_field(name="Game Description: ", value=description,inline=False)
+    embed.add_field(name="Game Description: ", value=fulldesc.text,inline=False)
     embed.add_field(name="Metacritic Score: ",value=f"**{metascore}**",inline=False)
     await message.edit(content=None, embed=embed)
+    driver.close()
 
 
 
