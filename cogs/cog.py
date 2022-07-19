7556d55aa20c04564b708907a61256abf4ee57b1
DarthJarJarJar <88474643+DarthJarJarJar@users.noreply.github.com>
2022-07-19 00:46:26 -0400

    please work

--- cogs/cog.py
+++ cogs/cog.py
@@ -71,7 +71,7 @@ class Game():
         platform_string = ", ".join(platforms)
 
         game_summary = game_data["summary"]
-        if len(game_summary)>3200:
+        if len(game_summary)>2800:
             game_summary = game_summary[:4000]+"..."
 
         self.platforms = platform_string
