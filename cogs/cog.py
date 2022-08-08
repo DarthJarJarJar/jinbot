fc31b3b8c80318d848e61244ba2f71c1c40d5e5f
DarthJarJarJar <88474643+DarthJarJarJar@users.noreply.github.com>
2022-08-08 11:50:15 -0400

    added SELF

--- cogs/cog.py
+++ cogs/cog.py
@@ -382,7 +382,7 @@ class cog(commands.Cog):
 
     @commands.hybrid_command()
     @app_commands.guilds(MY_GUILD_ID)
-    async def currency(ctx, convert:str, amount:int, to:str):
+    async def currency(self, ctx, convert:str, amount:int, to:str):
         url = f"https://api.apilayer.com/exchangerates_data/convert?to={convert}&from={to}&amount={amount}"
 
         payload = {}
